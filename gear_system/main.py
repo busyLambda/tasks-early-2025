6e3be3ab447899f0799ace4768c2495d3ad5c0ae
Susa Milán Mihály <mailtosnek@protonmail.com>
2025-05-24 11:28:33 +0200

    gear

--- gear_system/main.py
+++ gear_system/main.py
@@ -1,4 +1,76 @@
+DEBUG: bool = False
+
+def debug(msg: str) -> None:
+    if DEBUG:
+        print(f"DEBUG: {msg}")
+
+input: str = ""
 with open('./input.txt', 'r') as f:
   input = f.read()
 
-print(input)
+class Gear:
+    def __init__(self) -> None:
+        self.index = 2
+        self.values = [1, 2, 3]
+
+    def turn_forward(self) -> None:
+        if self.index+1 == len(self.values):
+            self.index=0
+        else:
+            self.index+=1
+
+def pull_lever(lever: list[int], gears: list[Gear]) -> None:
+    if len(gears) != len(lever):
+        debug("Lever-gear length mismatch!")
+
+    for i in range(len(lever)):
+        if lever[i] == 1:
+            gears[i].turn_forward()
+
+LEFT_LEVER: list[int] = [1, 1, 0]
+RIGHT_LEVER: list[int] = [0, 1, 1]
+
+def get_rotations(target: list[int], max_rotations: int = 8) -> str:
+    gears: list[Gear] = [
+        Gear(),
+        Gear(),
+        Gear(),
+    ]
+
+    left: int = target[0]
+    center: int = target[1]
+    right: int = target[2]
+
+    side_rotations: int = left+right
+    if side_rotations > max_rotations:
+        return "Megoldhatatlan"
+    for i in range(side_rotations):
+        pull_lever(LEFT_LEVER, gears)
+    if gears[1].values[gears[1].index] == center:
+        output: str = ""
+        for a in range(left):
+            output += "left "
+
+        for a in range(right):
+            output += "right "
+
+        return output
+    else:
+        return "Megoldhatatlan"
+
+def main() -> None:
+    for line in input.split("\n")[:-1]:
+        debug(line)
+        if len(line)>9:
+            x: list[str] = line.split("] ")
+            max_rotations: int = int(x[1])
+            target: list[int] = [int(y) for y in x[0].replace("[", "").split(", ")]
+
+            print(get_rotations(target, max_rotations))
+        else:
+            target: list[int] = [int(x) for x in line.strip("[]").split(", ")]
+            print(get_rotations(target))
+
+if __name__ == '__main__':
+    debug(f"\n---\n{input}---")
+    main()
