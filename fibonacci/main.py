d334387621807d4ce863c8c2ffed684e33d177bb
Susa Milán Mihály <mailtosnek@protonmail.com>
2025-05-26 19:19:46 +0200

    fib cc

--- fibonacci/main.py
+++ fibonacci/main.py
@@ -5,6 +5,15 @@ def debug(msg: str) -> None:
         print(f"DEBUG: {msg}")
 
 def parse_input_line(line: str) -> int | None:
+    """Átalakítja használható adattá az input egy sorát(pozitív egész szám).
+
+    Args:
+        line (str): Az input fájl egy sora.
+
+    Returns:
+        int: Siker esetén vissza adja a bemeneti soron lévő számot.
+        None: Hiba esetén None-t ad vissza.
+    """
     stripped_line = line.strip()
     if not stripped_line:
         return None
@@ -28,13 +37,26 @@ def filter_divisible_by_three(numbers: list[int]) -> list[int]:
     return [x for x in numbers if x % 3 == 0]
 
 def process_line(line: str) -> str:
+    """Feldolgoz egy sort az input-ból és vissza adja a megoldást.
+
+    Args:
+        line (str): az input fájl egy sora. Egyetlen pozitív egész számot tartalmaz.
+
+    Returns:
+       str: A feladat megoldása az adott bemeneti sorhoz(N/A ha nincs megoldás az inputhoz).
+    """
+    NO_SOLUTION: str = "N/A"
+
     n = parse_input_line(line)
     if n is None:
-        return "N/A"
+        return NO_SOLUTION
+
     fib_numbers = generate_fibonacci_up_to(n)
     filtered = filter_divisible_by_three(fib_numbers)
+
     if not filtered:
-        return "N/A"
+        return NO_SOLUTION
+
     return ", ".join(map(str, filtered))
 
 def main() -> None:
