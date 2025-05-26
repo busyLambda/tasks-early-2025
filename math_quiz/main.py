c632fe87d4676cc15bf4e437039e74e20825ec85
Susa Milán Mihály <mailtosnek@protonmail.com>
2025-05-26 19:33:32 +0200

    mislabeled task 3 :<

--- math_quiz/main.py
+++ math_quiz/main.py
@@ -7,7 +7,7 @@ def main() -> None:
     # R1,2 = 6+-sqrt(36-20)/2 = 5(✓), 1(✕ Nem lehetséges: R >= 2)
     print("1.: 5")
 
-    # 2. feladat
+    # 3. feladat
     # (1/(sqrt(1)+sqrt(2)))+(1/(sqrt(2)+sqrt(3)))+(1/(sqrt(3)+sqrt(4)))+...+(1/(sqrt(99)+sqrt(100)))
     #
     # (1/(sqrt(2)+sqrt(1)))+(1/(sqrt(3)+sqrt(2)))+(1/(sqrt(4)+sqrt(3)))+...+(1/(sqrt(100)+sqrt(99)))
